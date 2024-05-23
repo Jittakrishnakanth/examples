1a9b7f2425df4685fdc704e34a189b85b683c03d
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-05-23 15:24:14 +0530

    Add files via upload

---
+++ StringConcatenation.java
@@ -0,0 +1,13 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        // Define the strings to be concatenated
+        String str1 = "Hello, ";
+        String str2 = "World!";
+
+        // Concatenate using the concat() method
+        String result = str1.concat(str2);
+
+        // Output the concatenated string
+        System.out.println(result);
+    }
+}
