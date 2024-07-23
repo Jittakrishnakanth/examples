7378202304a7c29f6d1803f38f97d62d81c41805
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:52:02 +0530

    Add files via upload

---
+++ ExplicityTypecasting.java
@@ -0,0 +1,10 @@
+class ExplicityTypecasting 
+{
+	public static void main(String[] args) 
+	{
+		double pi = 3.1456789;
+		int duppi = (int)pi;
+		System.out.println("double pi="+pi);
+		System.out.println("int duppi="+duppi);
+	}
+}
