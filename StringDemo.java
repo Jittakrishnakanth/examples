bf22d0a8fe1c043dc16721c6f4f450078d684ce6
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:53:27 +0530

    Add files via upload

--- StringDemo.java
+++ StringDemo.java
@@ -1,15 +1,13 @@
-package krishna;
+class StringDemo
+{
+	public static void main(String[] args) 
+	{
+		String s1 = new String("java");
+		System.out.println("s1:"+s1);
+		ch = {'p','y','t','h','o','n'};
+		System.out.println("s2:"+s2);
+		String s3 = 'html';
+		System.out.println("s3:"+s3);
 
-public class StringDemo {
-		public static void main(String[] args) 
-		{
-			String s1 = new String("java");
-			System.out.println("s1:"+s1);
-			char ch[] = {'p','y','t','h','o','n'};
-			String s2 = new String("java");
-			System.out.println("s2:"+s2);
-			String s3 = "html";
-			System.out.println("s3:"+s3);
-
-		}
 	}
+}
