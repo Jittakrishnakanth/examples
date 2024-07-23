dedc0122c930effd12fd4a09b1f08906a8bd80d9
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:54:39 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+class TypeCasting 
+{
+	public static void main(String[] args) 
+	{
+		int salary = 50000;
+			double dupsalary = 50000;
+		System.out.println("salary="+salary);
+		System.out.println("dupsalary="+dupsalary);
+	}
+}
