2bac3d638003a02ca2de68d257798cfc845296e7
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:54:15 +0530

    Add files via upload

---
+++ TwoDarray.java
@@ -0,0 +1,20 @@
+class  TwoDarray
+{
+	public static void main(String[] args) 
+	{
+		String name[][] = new String[2][3];
+			name[0][0] = "krishna";
+		    name[0][1] = "Surya";
+			name[0][2] = "Mani";
+			name[1][0] = "Haari";
+			name[1][1] = "Manoj";
+			name[1][2] = "Akhi";
+			System.out.println("Names of students:");
+			System.out.println(name[0][0]);
+			System.out.println(name[0][1]);
+			System.out.println(name[0][2]);
+			System.out.println(name[1][0]);
+			System.out.println(name[1][1]);
+			System.out.println(name[1][2]);
+	}
+}
