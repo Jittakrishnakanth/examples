6cba4661bf4e98e97106af17736587282cef5b21
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:52:32 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,17 @@
+package com.arrays.oneD;
+public class Marks
+{
+ public static void main(String[] args) 
+	{
+	 int marks[] = new int[5];
+		 marks[0] = 55;
+		 marks[1] = 56;
+		 marks[2] = 57;
+		 marks[3] = 58;
+		 marks[4] = 59;
+		System.out.println("Marks of a students:");
+		for(int i=0;i<marks.length;i++){
+		System.out.println(marks[i]);
+		}	
+	}
+}
