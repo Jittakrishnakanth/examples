59faf85d8cdc8a5c87210fe6869d79e6d8589235
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:54:55 +0530

    Add files via upload

---
+++ TypeCastingExplicity.java
@@ -0,0 +1,10 @@
+class TypeCastingExplicity 
+{
+	public static void main(String[] args) 
+	{
+		double pi = 3.144367985;
+		int duppi = pi;/*here error becouse int direactly with out performing explicity typecasting*/
+		System.out.println("double pi="+pi);
+		System.out.println("pi="+pi);
+	}
+}
