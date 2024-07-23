f4c7d2d24691f90736527757ac6f5f944772e0df
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:49:51 +0530

    Add files via upload

---
+++ AvarageCalculator.java
@@ -0,0 +1,12 @@
+class AvarageCalculator 
+{
+	public static void main(String[] args) 
+	{
+		int a = 30;
+		int b = 40;
+		int c = 50;
+		int sum = 30+40+50;
+		double avg = sum/3.0;
+		System.out.println("doublesum="+avg);
+	}
+}
