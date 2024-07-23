02a37917b0f4f0fe57e52d73f2f341818e57caa2
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2024-07-23 18:50:54 +0530

    Add files via upload

---
+++ DatatypesDemo.java
@@ -0,0 +1,14 @@
+class DatatypesDemo 
+{
+	public static void main(String[] args) 
+	{
+		byte age=23;
+    System.out.println("age="+age);
+	   short year=2024;
+	   System.out.println("year="+year);
+	   int salary=50000;
+	    System.out.println("salary="+salary);
+		long pop=99999999l;
+		 System.out.println("population="+pop);
+	}
+}
